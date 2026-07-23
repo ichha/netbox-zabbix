422d97477c363502033e9d49ae6edb58d8dfb59a
ichha <thapa.ichha@gmail.com>
2026-07-23 07:44:25 +0545

    1

--- netbox_zabbix/__init__.py
+++ netbox_zabbix/__init__.py
@@ -7,6 +7,7 @@ class NetBoxZabbixConfig(PluginConfig):
     version = '0.1.0'
     author = 'Antigravity'
     author_email = 'antigravity@example.com'
+    base_url = 'zabbix'
     default_settings = {
         'zabbix_url': 'http://10.26.192.125/zabbix/api_jsonrpc.php',
         'zabbix_token': '7afab3979404434fd9a79841428d2a0cb77dce1cc3b0d4a28161e31259938c61',
--- netbox_zabbix/navigation.py
+++ netbox_zabbix/navigation.py
@@ -4,7 +4,8 @@ server_buttons = (
     PluginMenuButton(
         link='plugins:netbox_zabbix:zabbixserver_add',
         title='Add Zabbix Server',
-        icon_class='mdi mdi-plus-thick'
+        icon_class='mdi mdi-plus-thick',
+        permissions=['netbox_zabbix.add_zabbixserver']
     ),
 )
 
@@ -12,7 +13,8 @@ proxy_buttons = (
     PluginMenuButton(
         link='plugins:netbox_zabbix:zabbixproxy_add',
         title='Add Proxy',
-        icon_class='mdi mdi-plus-thick'
+        icon_class='mdi mdi-plus-thick',
+        permissions=['netbox_zabbix.add_zabbixproxy']
     ),
 )
 
@@ -20,56 +22,66 @@ hostgroup_buttons = (
     PluginMenuButton(
         link='plugins:netbox_zabbix:zabbixhostgroup_add',
         title='Add Hostgroup',
-        icon_class='mdi mdi-plus-thick'
+        icon_class='mdi mdi-plus-thick',
+        permissions=['netbox_zabbix.add_zabbixhostgroup']
     ),
 )
 
 servers = PluginMenuItem(
     link='plugins:netbox_zabbix:zabbixserver_list',
     link_text='Servers',
-    buttons=server_buttons
+    buttons=server_buttons,
+    permissions=['netbox_zabbix.view_zabbixserver']
 )
 
 proxies = PluginMenuItem(
     link='plugins:netbox_zabbix:zabbixproxy_list',
     link_text='Proxies',
-    buttons=proxy_buttons
+    buttons=proxy_buttons,
+    permissions=['netbox_zabbix.view_zabbixproxy']
 )
 
 proxy_groups = PluginMenuItem(
     link='plugins:netbox_zabbix:zabbixproxygroup_list',
-    link_text='Proxy Groups'
+    link_text='Proxy Groups',
+    permissions=['netbox_zabbix.view_zabbixproxygroup']
 )
 
 templates = PluginMenuItem(
     link='plugins:netbox_zabbix:zabbixtemplate_list',
-    link_text='Templates'
+    link_text='Templates',
+    permissions=['netbox_zabbix.view_zabbixtemplate']
 )
 
 template_groups = PluginMenuItem(
     link='plugins:netbox_zabbix:zabbixtemplategroup_list',
-    link_text='Template Groups'
+    link_text='Template Groups',
+    permissions=['netbox_zabbix.view_zabbixtemplategroup']
 )
 
 macros = PluginMenuItem(
     link='plugins:netbox_zabbix:zabbixmacro_list',
-    link_text='Macros'
+    link_text='Macros',
+    permissions=['netbox_zabbix.view_zabbixmacro']
 )
 
 tags = PluginMenuItem(
     link='plugins:netbox_zabbix:zabbixtag_list',
-    link_text='Tags'
+    link_text='Tags',
+    permissions=['netbox_zabbix.view_zabbixtag']
 )
 
 hostgroups = PluginMenuItem(
     link='plugins:netbox_zabbix:zabbixhostgroup_list',
     link_text='Hostgroups',
-    buttons=hostgroup_buttons
+    buttons=hostgroup_buttons,
+    permissions=['netbox_zabbix.view_zabbixhostgroup']
 )
 
 hosts = PluginMenuItem(
     link='plugins:netbox_zabbix:zabbixhost_list',
-    link_text='Hosts'
+    link_text='Hosts',
+    permissions=['netbox_zabbix.view_zabbixhost']
 )
 
 menu = PluginMenu(
